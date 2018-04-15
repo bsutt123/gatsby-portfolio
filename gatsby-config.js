1792688d0e458d91153a8dbcdd1e39bc111c8cfa
Brady <bsutt123@gmail.com>
2018-04-15 07:58:57 -0400

    include post css in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
+const cssnext = require('postcss-cssnext');
+const rucksack = require('rucksack-css');
+const short = require('postcss-short');
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-postcss-sass',
+      options:{
+        postCssPlugins: [
+          rucksack,
+          cssnext,
+          short,
+        ],
+        precision: 5,
+      }
+    },
+  ],
 }
